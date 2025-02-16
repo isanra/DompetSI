c259f7bd29b9f6366ad82cba2519fe56668d130f
isanra <ihsanr293@gmail.com>
2025-02-16 21:13:08 +0700

    DompetSI

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//heroconst
+text = "Selamat Datang Admin, di Rekening Sekolah impian.";
+let index = 0;
+const typingAnimation = document.getElementById("typing-animation");
+
+function type() {
+  if (index < text.length) {
+    typingAnimation.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, 50); // Kecepatan animasi
+  }
+}
+
+type();
